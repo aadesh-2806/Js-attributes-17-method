bbd047cd17692d6884b501e0a314a7d623ac549c
Aadesh Agrawal <74392722+aadesh-2806@users.noreply.github.com>
2021-07-16 23:04:24 +0530

    Add files via upload

---
+++ method.js
@@ -0,0 +1,55 @@
+/////////////////////////////////////////////////////methods//////////////////////////////////////////////////////////////
+
+/*
+foe eg:
+let hello='hh' ; hello.toUpperCase()
+"HH"
+
+const myn={
+    PI:3.141,
+    co:function (num){
+        return num*num;
+    }
+}
+
+myn.PI
+3.141
+
+myn.co
+ƒ (num){
+    return num*num;
+}
+
+const type = myn.co
+undefined
+
+type(4)
+16
+
+type(56)
+3136
+
+myn.co(5)
+25
+
+myn['co'](6)
+36
+
+const my={
+    PI:3.141,
+    co(num){
+        return num*num;
+    }
+}
+undefined
+my.co(5)
+25
+
+const myi={
+    PI:3.141,
+    co(){
+       console.log("dfg");
+    }
+}
+undefined
+*/
